236d4e90bb03ee616a85f19beeb478fd954ecb7e
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-02-22 20:45:28 -0500

    linted game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,95 +14,95 @@ $(document).ready(function() {
     $("#losses-text").html(`<p class="box-text">${losses}</p>`)
 
     // Preventing repeating values between buttons.
-    function valueCheck () {
+    function valueCheck() {
         if (cactuarVal === chocoboVal ||
             cactuarVal === moogleVal ||
             cactuarVal === moombaVal ||
             chocoboVal === moogleVal ||
             chocoboVal === moombaVal ||
             moogleVal === moombaVal) {
-                calculate()
-            }
+                calculate();
+                return
+            };
+        valueCheck()
     };
     
     // Define calculate function for use in win or lose event
-function calculate() {
-    cactuarVal = Math.floor(Math.random() * 11) + 1;
-    chocoboVal = Math.floor(Math.random() * 11) + 1;
-    moogleVal = Math.floor(Math.random() * 11) + 1;
-    moombaVal = Math.floor(Math.random() * 11) + 1;
-    valueCheck()
-    score = 0;
-    $("#score-text").html(`<p class="box-text">${score}</p>`)
-    goal = Math.floor(Math.random() * 101) + 19;
-    $("#goal-text").html(`<p class="box-text">${goal}</p>`)
-}
-
-// Win state: alert and iterate win, recalculate variables
-function win() {
-    wins ++
-    $("#wins-text").html(`<p class="box-text">${wins}</p>`)
-    calculate()
-    alert("Great job! You win!")
-}
-
-// Lose state: alert and iterate loss, recalculate variables
-function lose() {
-    losses ++
-    $("#losses-text").html(`<p class="box-text">${losses}</p>`)
-    calculate()
-    alert("Too bad! Try again!")
-}
-
-// Compare scores to determine win/loss state
-function checker() {
-    if (score === goal) {
-        win()
+    function calculate() {
+        cactuarVal = Math.floor(Math.random() * 11) + 1;
+        chocoboVal = Math.floor(Math.random() * 11) + 1;
+        moogleVal = Math.floor(Math.random() * 11) + 1;
+        moombaVal = Math.floor(Math.random() * 11) + 1;
+        valueCheck();
+        score = 0;
+        $("#score-text").html(`<p class="box-text">${score}</p>`)
+        goal = Math.floor(Math.random() * 101) + 19;
+        $("#goal-text").html(`<p class="box-text">${goal}</p>`)
+        console.log(cactuarVal)
+        console.log(chocoboVal)
+        console.log(moogleVal)
+        console.log(moombaVal)
     }
-    if (score > goal) {
-        lose()
+
+    // Win state: alert and iterate win, recalculate variables
+    function win() {
+        wins ++
+        $("#wins-text").html(`<p class="box-text">${wins}</p>`)
+        calculate()
+        alert("Great job! You win!")
+    };
+
+    // Lose state: alert and iterate loss, recalculate variables
+    function lose() {
+        losses ++
+        $("#losses-text").html(`<p class="box-text">${losses}</p>`)
+        calculate()
+        alert("Too bad! Try again!")
     }
-}
 
-// Cactuar button functionality
-$("#cactuar").on("click", function() {
-    score = score + cactuarVal;
-    $("#score-text").html(`<p class="box-text">${score}</p>`)
-    checker()
-});
+    // Compare scores to determine win/loss state
+    function checker() {
+        if (score === goal) {
+            win()
+        }
+        if (score > goal) {
+            lose()
+        }
+    };
 
+    // Cactuar button functionality
+    $("#cactuar").on("click", function() {
+        score = score + cactuarVal;
+        $("#score-text").html(`<p class="box-text">${score}</p>`)
+        checker()
+    });
 
-// Chocobo button functionality
-$("#chocobo").on("click", function() {
-    score = score + chocoboVal;
-    $("#score-text").html(`<p class="box-text">${score}</p>`)
-    checker()
-});
+    // Chocobo button functionality
+    $("#chocobo").on("click", function() {
+        score = score + chocoboVal;
+        $("#score-text").html(`<p class="box-text">${score}</p>`)
+        checker()
+    });
 
-// Moogle button functionality
-$("#moogle").on("click", function() {
-    score = score + moogleVal;
-    $("#score-text").html(`<p class="box-text">${score}</p>`)
-    checker()
-});
+    // Moogle button functionality
+    $("#moogle").on("click", function() {
+        score = score + moogleVal;
+        $("#score-text").html(`<p class="box-text">${score}</p>`)
+        checker()
+    });
 
+    // Moomba button functionality
+    $("#moomba").on("click", function() {
+        score = score + moombaVal;
+        $("#score-text").html(`<p class="box-text">${score}</p>`)
+        checker()
+    });
 
-// Moomba button functionality
-$("#moomba").on("click", function() {
-    score = score + moombaVal;
-    $("#score-text").html(`<p class="box-text">${score}</p>`)
-    checker()
-});
+    // Rearrange tiles on resize
+    $(window).on("resize", function() {
+        if (screen.width < 522) {
+            $("#statArea").prepend($(".winLose"));
+        }
+    });
 
-$(window).on("resize", function() {
-    if (screen.width < 522) {
-        $("#statArea").prepend($(".winLose"));
-    }
-    
-});
-
-console.log(moombaVal)
-console.log(moogleVal)
-console.log(chocoboVal)
-console.log(cactuarVal)
 });
